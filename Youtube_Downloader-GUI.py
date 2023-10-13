a9f7293952156e1afe519498f254ab129b6cce80
Jovan <93329694+Kinetikal@users.noreply.github.com>
2023-10-13 14:20:03 +0200

    Update Youtube_Downloader-GUI.py

--- Youtube_Downloader-GUI.py
+++ Youtube_Downloader-GUI.py
@@ -2,39 +2,65 @@ import PySimpleGUI as sg
 from pytube import YouTube
 import time
 import logging
+import threading
+import queue
 from os import listdir
 from os.path import isfile, join
 import os
 
 #====== Variables ======#
 
+# Create a queue for communication between threads
+update_queue = queue.Queue()
+
 #====== Functions ======#
-def download_youtube_video(YouTubeURL, Save_To_Path):
-    count = 0
-    for progress in range(1,100):
-        
-        window["-PBAR-"].update(0)
-        window["-PBAR-"].update(current_count = 0 + count)
-        
-        yt = YouTube(YouTubeURL, use_oauth=True, allow_oauth_cache=True)
+
+def download_youtube_video(YouTubeURL, output_path):
+    try:
+        def progress_callback(stream, chunk, remaining_bytes):
+            total_size = stream.filesize
+            bytes_downloaded = total_size - remaining_bytes
+            percentage = (bytes_downloaded / total_size) * 100
+            progress_text = f"Total Size {total_size} / {bytes_downloaded} Downloaded\n"
+            
+            window["-OUTPUT_WINDOW-"].update(progress_text)
+            window["-PBAR-"].update(percentage)
+
+        yt = YouTube(YouTubeURL, use_oauth=False, allow_oauth_cache=True, on_progress_callback=progress_callback)
+
         video = yt.streams.filter(file_extension="mp4")
-        download_video = video.download(output_path=Save_To_Path)
+        download_video = video.download(output_path=output_path)
         base,extension = os.path.splitext(download_video)
-        new_video_filename = base + "mp4"
+        new_video_filename = base + ".mp4"
         os.rename(download_video,new_video_filename)
-        window["-OUTPUT_WINDOW-"].print(f"{yt.title} has been successfully  downloaded.")
-        
-    window["-PBAR-"].update(0)
-        
-def download_youtube_audio(YouTubeURL, Save_To_Path):
-    yt = YouTube(YouTubeURL, use_oauth=True, allow_oauth_cache=True)
-    music = yt.streams.filter(only_audio=True).first()
-    download_music = music.download(output_path=Save_To_Path)
-    base,extension = os.path.splitext(download_music)
-    new_music_filename = base + "mp3"
-    os.rename(download_music,new_music_filename)
-    window["-OUTPUT_WINDOW-"].print(f"{yt.title} has been successfully downloaded.")
+        window["-OUTPUT_WINDOW-"].print(f"{yt.title}\nhas been successfully downloaded.\nSaved in {output_path}")
     
+    except Exception as e:
+        window["-OUTPUT_WINDOW-"].print(f"ERROR: {e}")
+        
+def download_youtube_audio(YouTubeURL, output_path):
+    try:
+        def progress_callback(stream, chunk, remaining_bytes):
+            total_size = stream.filesize
+            bytes_downloaded = total_size - remaining_bytes
+            percentage = (bytes_downloaded / total_size) * 100
+            progress_text = f"Total Size {total_size} / {bytes_downloaded} Downloaded\n"
+            
+            window["-OUTPUT_WINDOW-"].update(progress_text)
+            window["-PBAR-"].update(percentage)
+            
+        yt = YouTube(YouTubeURL, use_oauth=False,allow_oauth_cache=True,on_progress_callback=progress_callback)
+        
+        music = yt.streams.filter(only_audio=True).first()
+        download_music = music.download(output_path=output_path)
+        base,extension = os.path.splitext(download_music)
+        new_music_filename = base + ".mp3"
+        os.rename(download_music,new_music_filename)
+        window["-OUTPUT_WINDOW-"].print(f"{yt.title}\nhas been successfully downloaded.\nSaved in {output_path}")
+        
+    except Exception as e:
+        window["-OUTPUT_WINDOW-"].print(f"ERROR: {e}")
+        
 #====== GUI Theme ======#
 my_new_theme = {'BACKGROUND': '#1c1e23',
                 'TEXT': '#d2d2d3',
@@ -83,23 +109,34 @@ window = sg.Window("YouTube Downloader",layout,font="Arial 16",finalize=True)
 # Event loop to process "events" and get the "values" of the inputs
 while True:
     
-    event,values = window.read(timeout=1)
+    event,values = window.read(timeout = 200)
     
         #----Closing the programm with either option the [X] or just by pressing "Exit"----#
     if (event == sg.WIN_CLOSED or event == "-EXIT-"):
         break
     
     if event == "-BUTTON_DOWNLOAD_VIDEO-":
-        window.perform_long_operation(lambda:download_youtube_video(YouTubeURL = values["-VIDEO_INPUT-"],Save_To_Path = values["-SAVE_TO_FOLDER-"]),"-OUTPUT_WINDOW-")
+        if len(values["-SAVE_TO_FOLDER-"]) == 0:
+            window["-OUTPUT_WINDOW-"].update("Please enter a path where to save to the downloaded file.")
+        elif len(values["-VIDEO_INPUT-"]) == 0:
+            window["-OUTPUT_WINDOW-"].update("Please enter a Youtube link for Video Download.")
+        else:
+            download_thread = threading.Thread(target=download_youtube_video, args=(values["-VIDEO_INPUT-"], values["-SAVE_TO_FOLDER-"]))
+            download_thread.start()
+            #window.perform_long_operation(lambda:download_youtube_video(YouTubeURL = values["-VIDEO_INPUT-"],output_path = values["-SAVE_TO_FOLDER-"]),"-OUTPUT_WINDOW-")
         
     elif event == "-BUTTON_DOWNLOAD_AUDIO-":
-        window.perform_long_operation(lambda:download_youtube_audio(YouTubeURL = values["-AUDIO_INPUT-"],Save_To_Path = values["-SAVE_TO_FOLDER-"]),"-OUTPUT_WINDOW-")
-        
+        if len(values["-SAVE_TO_FOLDER-"]) == 0:
+            window["-OUTPUT_WINDOW-"].update("Please enter a path where to save to the downloaded file.")
+        elif len(values["-AUDIO_INPUT-"]) == 0:
+            window["-OUTPUT_WINDOW-"].update("Please enter a Youtube link for Audio Download.")
+        else:
+            download_thread = threading.Thread(target=download_youtube_audio, args=(values["-AUDIO_INPUT-"], values["-SAVE_TO_FOLDER-"]))
+            download_thread.start()
+            #window.perform_long_operation(lambda:download_youtube_audio(YouTubeURL = values["-AUDIO_INPUT-"],output_path = values["-SAVE_TO_FOLDER-"]),"-OUTPUT_WINDOW-")
+    
     elif event == "-CLEAR_OUTPUT-":
             window["-OUTPUT_WINDOW-"].update("")
-            window["-PBAR-"].update(current_count= 0 + 1)
-            time.sleep(0.5)
             window["-PBAR-"].update(0)
-        
-window.close()
     
+window.close()
